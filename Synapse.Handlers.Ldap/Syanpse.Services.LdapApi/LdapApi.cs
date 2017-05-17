cf8c8949dd0fd1cff15e6e8bbc202231ea1c1485
steveshortt <steve.shortt@gmail.com>
2017-05-17 18:00:19 -0500

    prototyping..

--- Synapse.Handlers.Ldap/Syanpse.Services.LdapApi/LdapApi.cs
+++ Synapse.Handlers.Ldap/Syanpse.Services.LdapApi/LdapApi.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Web.Http;
+
 using Synapse.Core;
 using Synapse.Services;
+using Syanpse.Services.LdapApi;
+using Synapse.Ldap.Core;
 
 [RoutePrefix( "lyra" )]
 public class LdapApiController : ApiController
@@ -26,27 +29,24 @@ public class LdapApiController : ApiController
         pe.DynamicParameters.Add( nameof( username ), username );
 
         long id = ec.StartPlan( pe, "getUser" );
-        StatusType status = await StatusHelper.GetStatusAsync( ec, id );
+        StatusType status = await StatusHelper.GetStatusAsync( ec, "getUser", id );
 
         return status == StatusType.Success ? (string)ec.GetPlanElements( "getUser", id, "Actions[0]:Result:ExitData" ) : null;
     }
-}
-class StatusHelper
-{
-    public static StatusType GetStatus(IExecuteController ec, long id)
+
+    [HttpGet]
+    [Route( "object/{type}/{name}" )]
+    public async Task<string> GetObject(ObjectClass type, string name)
     {
-        bool poll = true;
-        int result = 0;
-        while( poll )
-        {
-            System.Threading.Thread.Sleep( 1000 );
-            result = (int)ec.GetPlanElements( "getUser", id, "Status" );
-            poll = result < (int)StatusType.Success;
-        }
-        return (StatusType)result;
-    }
-    public static Task<StatusType> GetStatusAsync(IExecuteController ec, long id)
-    {
-        return Task.Run( () => GetStatus( ec, id ) );
+        IExecuteController ec = ExtensibilityUtility.GetExecuteControllerInstance();
+
+        StartPlanEnvelope pe = new StartPlanEnvelope() { DynamicParameters = new Dictionary<string, string>() };
+        pe.DynamicParameters.Add( nameof( name ), name );
+        pe.DynamicParameters.Add( nameof( type ), type.ToString() );
+
+        long id = ec.StartPlan( pe, "getObject" );
+        StatusType status = await StatusHelper.GetStatusAsync( ec, "getObject", id );
+
+        return status == StatusType.Success ? (string)ec.GetPlanElements( "getObject", id, "Actions[0]:Result:ExitData" ) : null;
     }
 }
---
+++ Synapse.Handlers.Ldap/Syanpse.Services.LdapApi/Utilities.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Synapse.Core;
+using Synapse.Services;
+
+
+namespace Syanpse.Services.LdapApi
+{
+    class StatusHelper
+    {
+        public static StatusType GetStatus(IExecuteController ec, string planName, long id)
+        {
+            int c = 0;
+            StatusType status = StatusType.New;
+            while( c < 30 )
+            {
+                System.Threading.Thread.Sleep( 1000 );
+                try { Enum.TryParse( ec.GetPlanElements( planName, id, "Result:Status" ).ToString(), out status ); } catch { }
+                c = status < StatusType.Success ? c + 1 : int.MaxValue;
+            }
+            return status;
+        }
+        public static Task<StatusType> GetStatusAsync(IExecuteController ec, string planName, long id)
+        {
+            return Task.Run( () => GetStatus( ec, planName, id ) );
+        }
+    }
+}
