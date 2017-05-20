7fa53ddba07b297b621ed0b7c1665be65d9e56a5
steveshortt <steve.shortt@gmail.com>
2017-05-20 16:21:15 -0500

    working on OUs

--- Synapse.Handlers.Ldap/Synapse.Ldap.Core/Runtime/OrgUnit.cs
+++ Synapse.Handlers.Ldap/Synapse.Ldap.Core/Runtime/OrgUnit.cs
@@ -11,16 +11,22 @@ namespace Synapse.Ldap.Core
             using( DirectoryEntry root = new DirectoryEntry( ldapRoot ) )
             using( DirectorySearcher searcher = new DirectorySearcher( root ) )
             {
-                searcher.Filter = $"(&(objectClass=organizationalUnit))(Name={name})))";
+                searcher.Filter = $"(&(objectClass=organizationalUnit))"; //(name={name})
                 searcher.SearchScope = SearchScope.Subtree;
+                searcher.PropertiesToLoad.Add( "name" );
                 searcher.PropertiesToLoad.Add( "distinguishedName" );
+                searcher.ReferralChasing = ReferralChasingOption.All;
 
-                SearchResult result = searcher.FindOne();
+                DirectoryEntry ou = null;
+                SearchResultCollection results = searcher.FindAll();
+                foreach( SearchResult result in results )
+                    if( result.Properties["name"][0].ToString().Equals( name, StringComparison.OrdinalIgnoreCase ) )
+                        ou = result.GetDirectoryEntry();
 
-                if( result == null )
+                if( ou == null )
                     return null;
                 else
-                    return new OrganizationalUnitObject( result.GetDirectoryEntry() );
+                    return new OrganizationalUnitObject( ou );
             }
         }
     }
