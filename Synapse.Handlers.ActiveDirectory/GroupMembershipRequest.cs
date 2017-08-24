e731a67df05746a7be682c7ce1dea5bc2c34f744
yangz1 <yeongtze@googlemail.com>
2017-08-23 22:21:33 -0400

    Removed codes for GroupMembershipHandler from the project.

--- Synapse.Handlers.ActiveDirectory/GroupMembershipRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-using Newtonsoft.Json;
-
-public class GroupMembershipRequest
-{
-    public List<AddSection> AddSection { get; set; }
-
-    public List<DeleteSection> DeleteSection { get; set; }
-}
