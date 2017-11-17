c722059b2b10aacd7ec173a7af962c683b8dd268
Guy Waguespack <guy.waguespack@gmail.com>
2017-11-17 15:26:38 -0600

    Created Core Tests for GroupManagement and Search.

---
+++ Synapse.ActiveDirectory.Tests/Core/GroupManagementTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.DirectoryServices.AccountManagement;
+using System.DirectoryServices;
+
+using NUnit.Framework;
+
+using Synapse.ActiveDirectory.Core;
+
+namespace Synapse.ActiveDirectory.Tests
+{
+    [TestFixture]
+    public class GroupManagementTests
+    {
+        DirectoryEntry workspace = null;
+        GroupPrincipal group = null;
+
+        [Test]
+        public void Core_GroupManagementTest()
+        {
+            // Setup Workspace
+            workspace = Utility.CreateWorkspace();
+            String workspaceName = workspace.Properties["distinguishedName"].Value.ToString();
+
+            // Create Group
+            group = Utility.CreateGroup( workspaceName );
+
+            // Add User To Group
+            UserPrincipal user = Utility.CreateUser( workspaceName );
+            UserPrincipalObject upo = DirectoryServices.GetUser( user.DistinguishedName, true, false, false );
+            int beforeCount = upo.Groups.Count;
+            Console.WriteLine( $"Adding User [{user.Name}] To Group [{group.Name}]." );
+            DirectoryServices.AddUserToGroup( user.DistinguishedName, group.DistinguishedName );
+            upo = DirectoryServices.GetUser( user.DistinguishedName, true, false, false );
+            int afterCount = upo.Groups.Count;
+            Assert.That( afterCount, Is.EqualTo( beforeCount + 1 ) );
+
+            // Remove User From Group
+            beforeCount = afterCount;
+            Console.WriteLine( $"Removing User [{user.Name}] From Group [{group.Name}]." );
+            DirectoryServices.RemoveUserFromGroup( user.DistinguishedName, group.DistinguishedName );
+            upo = DirectoryServices.GetUser( user.DistinguishedName, true, false, false );
+            afterCount = upo.Groups.Count;
+            Assert.That( afterCount, Is.EqualTo( beforeCount - 1 ) );
+
+            // Delete User
+            Utility.DeleteUser( user.DistinguishedName );
+
+            // Add Group To Group
+            GroupPrincipal newGroup = Utility.CreateGroup( workspaceName );
+            GroupPrincipalObject gpo = DirectoryServices.GetGroup( newGroup.DistinguishedName, true, false, false );
+            beforeCount = gpo.Groups.Count;
+            Console.WriteLine( $"Adding Group [{newGroup.Name}] To Group [{group.Name}]." );
+            DirectoryServices.AddGroupToGroup( newGroup.DistinguishedName, group.DistinguishedName );
+            gpo = DirectoryServices.GetGroup( newGroup.DistinguishedName, true, false, false );
+            afterCount = gpo.Groups.Count;
+            Assert.That( afterCount, Is.EqualTo( beforeCount + 1 ) );
+
+            // Remove Group From Group
+            beforeCount = afterCount;
+            Console.WriteLine( $"Removing Group [{newGroup.Name}] From Group [{group.Name}]." );
+            DirectoryServices.RemoveGroupFromGroup( newGroup.DistinguishedName, group.DistinguishedName );
+            gpo = DirectoryServices.GetGroup( newGroup.DistinguishedName, true, false, false );
+            afterCount = gpo.Groups.Count;
+            Assert.That( afterCount, Is.EqualTo( beforeCount - 1 ) );
+
+            // Delete Groups
+            Utility.DeleteGroup( newGroup.DistinguishedName );
+            Utility.DeleteGroup( group.DistinguishedName );
+
+            // Cleanup Workspace
+            Utility.DeleteWorkspace( workspaceName );
+        }
+    }
+}
---
+++ Synapse.ActiveDirectory.Tests/Core/SearchTests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.DirectoryServices.AccountManagement;
+using System.DirectoryServices;
+
+using NUnit.Framework;
+
+using Synapse.ActiveDirectory.Core;
+
+namespace Synapse.ActiveDirectory.Tests
+{
+    [TestFixture]
+    public class SearchTests
+    {
+        DirectoryEntry workspace = null;
+
+        [Test]
+        public void Core_SearchTest()
+        {
+            // Setup Workspace
+            workspace = Utility.CreateWorkspace();
+            String workspaceName = workspace.Properties["distinguishedName"].Value.ToString();
+
+            // Create Users
+            UserPrincipal up1 = Utility.CreateUser( workspaceName );
+            UserPrincipal up2 = Utility.CreateUser( workspaceName );
+            UserPrincipal up3 = Utility.CreateUser( workspaceName );
+
+            // Create Groups
+            GroupPrincipal gp1 = Utility.CreateGroup( workspaceName );
+            GroupPrincipal gp2 = Utility.CreateGroup( workspaceName );
+
+            // Search For Users
+            Console.WriteLine( $"Searching For Users In [{workspaceName}]." );
+            string[] properties = new string[] { "name", "objectGUID", "objectSid" };
+            SearchResults results = DirectoryServices.Search( workspaceName, @"(objectClass=User)", properties );
+            Assert.That( results.Results.Count, Is.EqualTo( 3 ) );
+            foreach ( SearchResultRow row in results.Results )
+            {
+                Console.WriteLine( $"  >> [{row.Path}]" );
+                Assert.That( row.Properties.ContainsKey( "name" ), Is.True );
+                Assert.That( row.Properties["name"], Is.Not.Null );
+                Assert.That( row.Properties.ContainsKey( "objectGUID" ), Is.True );
+                Assert.That( row.Properties["objectGUID"], Is.Not.Null );
+                Assert.That( row.Properties.ContainsKey( "objectSid" ), Is.True );
+                Assert.That( row.Properties["objectSid"], Is.Not.Null );
+            }
+
+            // Search For Groups
+            Console.WriteLine( $"Searching For Groups In [{workspaceName}]." );
+            results = DirectoryServices.Search( workspaceName, @"(objectClass=Group)", properties );
+            Assert.That( results.Results.Count, Is.EqualTo( 2 ) );
+            foreach ( SearchResultRow row in results.Results )
+            {
+                Console.WriteLine( $"  >> [{row.Path}]" );
+                Assert.That( row.Properties.ContainsKey( "name" ), Is.True );
+                Assert.That( row.Properties["name"], Is.Not.Null );
+                Assert.That( row.Properties.ContainsKey( "objectGUID" ), Is.True );
+                Assert.That( row.Properties["objectGUID"], Is.Not.Null );
+                Assert.That( row.Properties.ContainsKey( "objectSid" ), Is.True );
+                Assert.That( row.Properties["objectSid"], Is.Not.Null );
+            }
+
+
+            // Delete Users
+            Utility.DeleteUser( up1.DistinguishedName );
+            Utility.DeleteUser( up2.DistinguishedName );
+            Utility.DeleteUser( up3.DistinguishedName );
+
+            // Delete Groups
+            Utility.DeleteGroup( gp1.DistinguishedName );
+            Utility.DeleteGroup( gp2.DistinguishedName );
+
+            // Cleanup Workspace
+            Utility.DeleteWorkspace( workspaceName );
+
+        }
+
+    }
+}
